19344ae4d5f446cdef84b2e2bd406cea99378a5d
slmnfqh <jhokamfaqih5@gmail.com>
2023-12-14 07:26:13 +0700

    initial commit 44

--- src/routes/universitas.js
+++ src/routes/universitas.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const Controller = require("../controllers/universitas");
 
-router.get("/cari", Controller.searchUniversity);
+router.post("/cari", Controller.searchUniversity);
 router.get("/detail", Controller.universityDetail);
 router.get("/prodi", Controller.prodiAtUniversity);
 router.get("/cari/prodi", Controller.searchProdi);
